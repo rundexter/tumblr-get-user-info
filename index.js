216aa4d35766aeb7b6f9aa7d99bc9537320d0e2c
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-24 14:07:56 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,41 @@
+var _ = require('lodash'),
+    util = require('./util.js');
+
+var request = require('request').defaults({
+    baseUrl: 'https://api.tumblr.com/v2/'
+});
+
+var pickOutputs = {
+        'following': 'response.user.following',
+        'default_post_format': 'response.user.default_post_format',
+        'name': 'response.user.name',
+        'likes': 'response.user.likes',
+        'blogs': 'response.user.blogs'
+    };
+
 module.exports = {
+    /**
+     * Return auth params.
+     *
+     * @param dexter
+     * @returns {*}
+     */
+    authOptions: function (dexter) {
+        var oauth = {
+            consumer_key: dexter.environment('tumblr_consumer_key'),
+            consumer_secret: dexter.environment('tumblr_consumer_secret'),
+            token: dexter.environment('tumblr_token'),
+            token_secret: dexter.environment('tumblr_token_secret')
+        };
+
+        return (
+            oauth.consumer_key &&
+            oauth.consumer_secret &&
+            oauth.token &&
+            oauth.token_secret
+        )? oauth : false;
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +43,26 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var oauth = this.authOptions(dexter),
+            uriLink = 'user/info';
+
+        if (!oauth)
+            return this.fail('A [tumblr_consumer_key,tumblr_consumer_secret,tumblr_token,tumblr_token_secret] environment need for this module.');
+
+        //send API request
+        request.get({
+            url: uriLink,
+            oauth: oauth,
+            json: true
+        }, function (error, response, body) {
+            if (error)
+                this.fail(error);
+
+            else if (_.parseInt(response.statusCode) !== 200)
+                this.fail(body);
+
+            else
+                this.complete(util.pickResult(body, pickOutputs));
+        }.bind(this));
     }
 };
